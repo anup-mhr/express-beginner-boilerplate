8ed10712d663174a14481774df6e81e38f25cee1
Anup Maharjan <anupmhrzn16@gmail.com>
2024-06-25 13:55:41 +0545

    feat: add morgan

--- src/app.js
+++ src/app.js
@@ -1,9 +1,20 @@
 const express = require('express');
+const morgan = require('./config/morgan');
+const config = require('./config/config');
 
 const app = express();
 
 //MIDDLEWARES
-app.use(express.json()); //for parsing json
+if (config.env !== 'test') {
+  app.use(morgan.successHandler);
+  app.use(morgan.errorHandler);
+}
+
+//for parsing json
+app.use(express.json());
+
+//for reading form data
+app.use(express.urlencoded({ extended: false }));
 
 //ROUTES
 
